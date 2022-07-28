e3342bd692dbbdce8c9902b03449c8ff0795b884
Jefferson Pan Wei Quan <jpwq09.21@ichat.sp.edu.sg>
2022-07-29 01:15:56 +0800

    COMMITING AGAIN

---
+++ Grp5MiniProj/vacancyfile.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stack>
+#include <list>
+
+using namespace std;
+
+
+class Vacancies {
+public:
+	Vacancies(string VacanciesInfo); //Constructer for Vacancies 
+	string getCCAName();
+	int getvacancies();
+	char getCCAID();
+private:
+	string CCAName;
+	int vacancies;
+	char CCAID;
+};
+
+class vacancieslist {
+public:
+
+	vacancieslist(string pathtovacancies);
+
+	vector<Vacancies> ccavacancy;
+
+};
