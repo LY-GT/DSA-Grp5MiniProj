e59509751530df8f80285d2140bebc12d3a234f0
Lucas Yip <lucasyiphh14.21@ichat.sp.edu.sg>
2022-07-31 00:29:16 +0800

    Merged Jeff and Jeanna

--- Grp5MiniProj/Grp5MiniProj.cpp
+++ Grp5MiniProj/Grp5MiniProj.cpp
@@ -1,61 +1,216 @@
-// Grp5MiniProj.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <string>
+#include <algorithm>
 #include "SpStudent.h"
+#include "vacancyfile.h"
+
 using namespace std;
 
-/*void fileinput()
-{
-    char filename[100];
-    char defaultpath[100]{ d,e,f,a,u,l,t };
-    cout << "Enter a file path, if you want to use the default path, type default" << endl;
-    cout << "File name: ";
-    cin.getline(filename, 99);
-    if filename = defaultpath
-    {
-        list list("c:\\Users\\USER\\Documents\\studentinfo.txt");
-    }
-    else
-    {
-        list list(filename);
-    }
-    //Ive not gotten this to work yet, if you want to attempt fixing this go ahead, I cant figure out the issue yet
-}*/
 
-void test(SpStudent student)
-{
-    cout << "First name: " << student.getfirstname() << endl;
-    cout << "Last name: " << student.getlastname() << endl;
-    cout << "GPA: " << student.getGPA() << endl;
-    cout << "Full name: " << student.getname() << endl;
-    cout << endl;
-    //this is a test for file reading, use it if you want to, 
-    //but look in SpStudent.h first to // the private part of the list class
+bool sortbyGPA(student_class& A, student_class& B) {
+
+	char club = 'g'; //sort does not allow carry value.
+
+	if (A.getChoice1() == club && B.getChoice1() == club)
+	{
+		return (A.getWinchoice1() > B.getWinchoice1());
+	}
+	else if (A.getChoice1() == club && B.getChoice1() == club)
+	{
+		return (A.getGPA() > B.getGPA());
+	}
+	else if (A.getChoice1() == club)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
 }
 
-int main()
-{
-    list list("C:\\Users\\USER\\Documents\\studentinfo.txt");
-    //when pathing to your file, use double \ for every path. Its a thing with fstream idk.
-    // so if your file is in C:\Users\Name\Documents, type it as C:\\Users\\Name\\Documents\\(filename).txt
-    //fileinput();
-    for (SpStudent s : list.students)
-        test(s);
+//TO PRINT OUTPUT
+void test(student_class student_class) {
 
-    
-    return 0;
+	cout << student_class.getFirst_name() << " ";
+	cout << student_class.getLast_name() << " ";
+	cout << student_class.getGPA() << " ";
+	cout << student_class.getChoice1() << " ";
+	cout << student_class.getChoice2() << " ";
+	cout << student_class.getChoice3() << " ";
+	cout << student_class.getWinchoice1() << " ";
+	cout << student_class.getWinchoice2() << " ";
+	cout << student_class.getWinchoice3() << " " << endl;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	//student_class student_class("John Joe 3.0 g");
+	studentlist filelist("C:\\Users\\jeann\\Documents\\studentinfo.txt");
+	vacancieslist vacancyfile("C:\\Users\\KalliasWolf\\Documents\\Vacancy.txt");
+
+
+	//Assigning vacancies to variables
+	int Dancingvacancies;
+	int Tennisvacancies;
+	int Gamingvacancies;
+	int Runningvacancies;
+	int swimmingvacancies;
+	int chessvacancies;
+	string specialcca;
+	for (int i = 0; i < vacancyfile.ccavacancy.size(); i++) {
+		Vacancies v = vacancyfile.ccavacancy.at(i);
+		if (v.getCCAID() == 'D') {
+			Dancingvacancies = v.getvacancies();
+		}
+		if (v.getCCAID() == 'T') {
+			Tennisvacancies = v.getvacancies();
+		}
+		if (v.getCCAID() == 'G') {
+			Gamingvacancies = v.getvacancies();
+		}
+		if (v.getCCAID() == 'R') {
+			Runningvacancies = v.getvacancies();
+		}
+		if (v.getCCAID() == 'S') {
+			swimmingvacancies = v.getvacancies();
+		}
+		if (v.getCCAID() == 'C') {
+			chessvacancies = v.getvacancies();
+		}
+		if (v.getCCAID() == 'G') {
+			specialcca = v.getCCAName();
+		}
+	}
+	//cout << specialcca << endl;
+
+
+
+	//Print out contents of list
+	/*for (int i = 0; i < vacancyfile.ccavacancy.size(); i++) {
+		Vacancies v = vacancyfile.ccavacancy.at(i);
+		print(v);
+	}*/
+
+
+
+
+	//change the above if you want to change ^^^
+
+//*********************************
+	// I made 2 versions to find the 1st choice, LIST and VECTOR
+	// Both are valid. I don't know which is better. 
+	// Pick the one that works :D
+
+//======================================================
+//  LIST VERSION OF FINDING 1ST CHOICE
+
+/*
+	for (student_class s : filelist.find1stClub_list(filelist, 'g'))
+		test(s);
+
+	list<student_class> chesslist_1 = filelist.find1stClub_list(filelist, 'g');
+	list<student_class> chess_list_wins=filelist.replaceWins(chesslist_1,'g'); //NOT DONE
+	//^^Can combine these 2 together tbh, but it will be messy :D^^
+
+	//After making a list of 1st choice and 3 winning records people
+	//It is time to update the master file, aka remove people in the list
+	//we just made from the whole population list.
+
+
+	list<student_class> remainder = filelist.remainding(filelist, chess_list_wins);
+	//^^This is to find the remainding people with no given club^^ NOT DONE
+
+*/
+
+
+
+//===========================================================
+// 	VECTOR VERSION OF FINDING 1ST CHOICE(Jeffs code)
+
+	//Prints those who have chess as 1st choice (VECTOR)
+	//for (student_class s : filelist.find1stClub(filelist, 'g',Gamingvacancies))
+		//test(s);
+
+
+	vector<student_class> gaminglist_nowins = filelist.find1stClub(filelist, 'g', Gamingvacancies);
+
+	for (int i = 0; i < gaminglist_nowins.size(); i++) {
+		student_class s = gaminglist_nowins.at(i);
+		test(s);
+	}
+
+	//cout << filelist.students.size() << endl;
+	//cout << vacancyfile.ccavacancy.size() << endl;
+
+	//vector<student_class> chesslist_wins = filelist.replaceWins_vector(chesslist_nowins, 'g');
+	//vector<student_class> remainder = filelist.remainding_vector(filelist, chesslist_wins);
+
+	//Printing Out CCA + ID + Vacancies
+	//cout << "Name of CCA " << "	" << "CCAID " << "	" << "Number of Vacancies " << endl;
+	//cout << "======================================================================" << endl;
+
+
+
+		//Prints the whole student list
+		/*for (int i = 0; i < filelist.students.size(); i++)
+		{
+			student_class s = filelist.students.at(i);
+			test(s);
+		}*/
+
+
+
+
+	return 0;
+
+
+	/*//===========================================================
+	// 	VECTOR VERSION OF FINDING 1ST CHOICE(Jeanna's code)
+		//Prints those who have chess as 1st choice (VECTOR)
+
+		//SORT BY G CLUB, WIN RECORD THEN GPA.
+		sort(filelist.students.begin(), filelist.students.end(), sortbyGPA);
+
+		cout << "Sorted by g club, win choice then GPA\n";
+
+		for (int i = 0; i < filelist.students.size() - 1; i++)
+		{
+			student_class s = filelist.students.at(i);
+			test(s);
+		}
+		cout << "\n====================================\n\nOnly 2 vacancy\n";
+
+		for (student_class s : filelist.find1stClub(filelist, 'g'))
+			test(s);
+
+
+		vector<student_class> chesslist_nowins = filelist.find1stClub(filelist, 'g');
+		//vector<student_class> chesslist_wins = filelist.replaceWins_vector(chesslist_nowins, 'g');
+		//vector<student_class> remainder = filelist.remainding_vector(filelist, chesslist_wins);
+
+
+
+
+
+
+	///////////////////////////////////////////////////////////
+
+		/*
+		//Prints the whole student list
+		for (int i = 0; i < filelist.students.size(); i++)
+		{
+			student_class s = filelist.students.at(i);
+			test(s);
+		}
+		*/
+		///////////////////////////////////////////////////////
+
+
+	return 0;
+
+	*/
+}
