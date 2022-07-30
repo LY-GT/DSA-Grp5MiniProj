53cb63b757cccdb90cfe77a415ad13d13d8319c8
Lucas Yip <lucasyiphh14.21@ichat.sp.edu.sg>
2022-07-31 00:29:16 +0800

    Merged Jeff and Jeanna

---
+++ Grp5MiniProj/vacancyfile.cpp
@@ -0,0 +1,49 @@
+#include "vacancyfile.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+#include <list>
+#include "SpStudent.h"
+
+
+using namespace std;
+
+Vacancies::Vacancies(string VacanciesInfo) {
+	stringstream VacancyStream(VacanciesInfo);
+	VacancyStream >> CCAName;
+	VacancyStream >> CCAID;
+	VacancyStream >> vacancies;
+}
+
+string Vacancies::getCCAName()
+{
+	return CCAName;
+}
+char Vacancies::getCCAID()
+{
+	return CCAID;
+}
+int Vacancies::getvacancies()
+{
+	return vacancies;
+}
+//Reading the file
+vacancieslist::vacancieslist(string pathtovacancies) {
+	ifstream vacancypath(pathtovacancies);
+
+	string vacancycontents;
+
+	while (!vacancypath.eof()) {
+		getline(vacancypath, vacancycontents);
+
+		Vacancies v(vacancycontents);
+		ccavacancy.push_back(v); //storing vacancies.txt into vector
+		//cout << ccavacancy.size() << endl;
+	}
+	vacancypath.close();
+}
+
+//Check winchoice
